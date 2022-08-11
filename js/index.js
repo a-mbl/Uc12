16a20c3fdb92b861cc18f690f3d539e8359124f4
Ale <aleymblira@gmail.com>
2022-08-11 13:15:27 -0300

    Game Mania v03.2

--- js/index.js
+++ js/index.js
@@ -1,25 +1,2 @@
-setInterval(function(){
-    
-    let novaHora = new Date();
-    // getHours trará a hora
-    // geMinutes trará os minutos
-    // getSeconds trará os segundos
-    let hora = novaHora.getHours();
-    let minuto = novaHora.getMinutes();
-    let segundo = novaHora.getSeconds();
-    // Chamamos a função zero para que ela retorne a concatenação
-    // com os minutos e segundos
-    minuto = zero(minuto);
-    segundo = zero(segundo);
-    // Com o textContent, iremos inserir as horas, minutos e segundos
-    // no nosso elemento HTML
-    document.getElementById('hora').textContent = hora+':'+minuto+':'+segundo;
-},1000)
-
-// para mostrar hora no formato padrão:
-
-function zero(x) {
-    if (x < 10) {
-        x = '0' + x;
-    } return x;
-}
+alert('Boas vindas ao nosso portal!');
+console.log('Bem-vindo ao nosso portal!')
